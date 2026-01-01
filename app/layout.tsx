4ab05bbe5849a3297e0b49f6db198e610f8c143c
Karim Shehadeh <karim@karim.cloud>
2026-01-01 13:05:32 -0500

    Add @vercel/analytics for improved analytics tracking
    - Integrated @vercel/analytics version 1.6.1 into the project for enhanced analytics capabilities.
    - Updated layout to include the Analytics component within the SessionProvider for tracking user interactions.
    - Made minor adjustments to the admin and user dropdown components for consistency.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          {children}
+          <Analytics />
+        </SessionProvider>
       </body>
     </html>
   );
--- components/user-dropdown.tsx
+++ components/user-dropdown.tsx
@@ -96,3 +96,4 @@ export function UserDropdown({ name, image }: UserDropdownProps) {
 }
 
 
+
